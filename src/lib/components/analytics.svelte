b05ca71271a636adf3f973906b429923eefaa529
DiceBreakers <141854239+DiceBreakers@users.noreply.github.com>
2023-12-05 10:51:21 -0600

    Analytics

--- src/lib/components/analytics.svelte
+++ src/lib/components/analytics.svelte
@@ -12,19 +12,17 @@
   </script>
   
   <svelte:head>
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-SVF8GLGDEK">
-    </script>
-    <script>
-      window.dataLayer = window.dataLayer || []
-  
-      function gtag() {
-        dataLayer.push(arguments)
-      }
-  
-      gtag('js', new Date())
-      gtag('config', 'MEASUREMENT_ID')
-    </script>
+  <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-TVWNT2XZ');</script>
+  <!-- End Google Tag Manager -->
   </svelte:head>
   
+
+  <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVWNT2XZ"
+  height="0" width="0" style="display:none;visibility:hidden" title="GoogleTag"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
--- src/routes/about/about.svelte
+++ src/routes/about/about.svelte
@@ -3,15 +3,15 @@
 		<div class="card p-4">
             <h2><b>About Dice Breakers:</b></h2>							
             <p>Hmm. Well I originally had this section hidden in the HTML, but apparently google does not approve of that? I don't know. I thought it was more fun that way.
-            Anyway, my website doesn't have very many words (outside the database), but I'm told I need to add some so the robots that control the Internet know what happens here.
+            Anyway, my website doesn't have very many words (outside the database), but I'm told I need to add some so the robots know what happens here.
             Dice Breakers started as game I made up that used a google sheet and some dice to pick activities when friends and I were indecisive.
-            At first it was mostly activity based, then a friend suggested adding topics of conversation to the list of activities, which was a great addition!
+            At first it was mostly activity based, then a friend suggested adding topics of conversation to the list of activities. It was a great addition!
             Anyway, I couldn't sleep one night and was writing questions to add to the game, then the name D(ice) Breakers came to mind.
-            I guess I liked the name enough to learn some web development and turn it from a Google Sheets game to a website?</p>
+            Maybe I was sleep deprived, but I thought it was clever. I know I'm not the first person to turn the pun into a mini-game, but I do think I've put the most hours into developing the concept!</p>
             <br>
-            <p>My goal is to provide a platform for positive, thought provoking, or otherwise meaningful conversations. In person, online, wherever works to get people to connect and think a little bit more!
-            Please, share this game with your friends! I hope I've added enough initial prompts to be a functional foundation. And ideally, eventually, this website is known to people outside my family and prompts are generated more organically.
-            I'm by no means an experienced developer, so I'm figuring things out as I go. I think I've done okay, but if you have suggestions, I'm very receptive to ideas!</p>
+            <p>My goal is to provide a platform for positive, thought provoking, or meaningful conversations. In person, online, wherever works to get people to connect and talk a little bit more!
+            I think we've added enough initial prompts to be a functional foundation for the game. And ideally, eventually, this website is known to people outside my family and prompts are generated more organically.
+            I'm by no means an experienced developer, so I'm figuring things out as I go. I think I've done okay, but if you have suggestions, I'm very receptive to community input! I don't know if anyone has even read this, but if you do have any suggestions please email me at <a href="mailto:suggestions@DiceBreakers.app">suggestions@DiceBreakers.app!</a></p>
         </div>
     </div>
 </div>
