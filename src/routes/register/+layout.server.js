6522d1eda5bc1ffbf6d6838122c907157aa4a62c
DiceBreakers <141854239+DiceBreakers@users.noreply.github.com>
2023-12-17 22:25:16 -0600

    RegisterFix

--- src/routes/register/+layout.server.js
+++ src/routes/register/+layout.server.js
@@ -1,8 +1,7 @@
 import { redirect } from '@sveltejs/kit';
 
-export const load = async ({ locals }) => {
-
-	if (!locals.pb.authStore.isValid) {
-		throw redirect(303, '/login');
+export const load = ({ locals }) => {
+	if (locals.pb.authStore.isValid) {
+		throw redirect(303, '/profile');
 	}
 };
