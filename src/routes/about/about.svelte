0159176ba48c37ff4f2f256f9efbacfbfdc1be0f
DiceBreakers <141854239+DiceBreakers@users.noreply.github.com>
2023-12-19 11:53:13 -0600

    About Changes

--- src/routes/about/about.svelte
+++ src/routes/about/about.svelte
@@ -9,9 +9,11 @@
             Anyway, I couldn't sleep one night and was writing questions to add to the game, then the name D(ice) Breakers came to mind.
             Maybe I was sleep deprived, but I thought it was clever. I know I'm not the first person to turn the pun into a mini-game, but I do think I've put the most hours into developing the concept!</p>
             <br>
-            <p>My goal is to provide a platform for positive, thought provoking, or meaningful conversations. In person, online, wherever works to get people to connect and talk a little bit more!
+            <p>I want this website to provide a platform for positive, thought provoking, or meaningful conversations. In person, online, wherever works to get people to connect and talk a little bit more!
             I think we've added enough initial prompts to be a functional foundation for the game. And ideally, eventually, this website is known to people outside my family and prompts are generated more organically.
             I'm by no means an experienced developer, so I'm figuring things out as I go. I think I've done okay, but if you have suggestions, I'm very receptive to community input! I don't know if anyone has even read this, but if you do have any suggestions please email me at <a href="mailto:suggestions@DiceBreakers.app">suggestions@DiceBreakers.app!</a></p>
+            <br>
+            <p>I'm getting way ahead of myself here, but I have two big goals in life. First, I'd like to not have to worry about money so I can build and tinker with whatever projects come to mind. And second, I want to be able to help people. I hope Dice Breakers can help me accomplish both these things, and have set up the Dice Breakers LLC to be equally committed to both goals. As such, Dice Breakers has pledged that a minimum of 50% of profit earned through this site will be donated to charity. Now I just need to make it profitable!</p>
         </div>
     </div>
 </div>
